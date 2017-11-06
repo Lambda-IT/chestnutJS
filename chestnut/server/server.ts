2af7dfe10100195b187c1ee8adfb3a23d7c67ad1
SusanneBoghossian <susanne.boghossian@lambda-it.ch>
2017-11-06 15:22:19 +0100

    mongo

--- chestnut/server/server.ts
+++ chestnut/server/server.ts
@@ -1,43 +1,39 @@
-import * as express from "express";
-import * as bodyParser from "body-parser";
-
-import { graphqlExpress, graphiqlExpress } from "apollo-server-express";
-import { initGraphQLSchema } from "./app/schema";
-const WS_GQL_PATH = "/subscriptions";
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as mongoose from 'mongoose';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import { initGraphQLSchema } from './app/schema';
+const WS_GQL_PATH = '/subscriptions';
 
 export type ChestnutOptions = {
-  port: number;
-  models: any;
-  // connection string for mongo
-  // read in typegoose for mongo connection
+    port: number;
+    models: any;
+    dbConnection: string;
 };
 
 export type Chestnut = {
-  expressApp: any;
+    expressApp: any;
 };
 
 export function initChestnut(options: ChestnutOptions): Chestnut {
-  const app = express();
+    const app = express();
 
-  app.use("/graphql", function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Content-Type, Authorization, Content-Length, X-Requested-With"
-    );
-    if (req.method === "OPTIONS") {
-      res.sendStatus(200);
-    } else {
-      next();
-    }
-  });
-  const schema = initGraphQLSchema(options.models);
-  app.use("/graphql", bodyParser.json(), graphqlExpress({ schema }));
-  app.get("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
+    app.use('/graphql', function(req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+        if (req.method === 'OPTIONS') {
+            res.sendStatus(200);
+        } else {
+            next();
+        }
+    });
 
-  app.listen(options.port, () =>
-    console.log(`simple-todos-server listening on port ${options.port}`)
-  );
+    mongoose.connect(options.dbConnection);
+    const schema = initGraphQLSchema(options.models);
+    app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+    app.get('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
-  return { expressApp: app };
+    app.listen(options.port, () => console.log(`simple-todos-server listening on port ${options.port}`));
+
+    return { expressApp: app };
 }
--- external-app/index.ts
+++ external-app/index.ts
@@ -2,5 +2,5 @@ import { Chestnut, ChestnutOptions, initChestnut } from '../chestnut';
 import * as models from './model/models';
 
 const PORT = +(process.env.PORT || 9000);
-
-const server = initChestnut({port: PORT, models: models });
+const dbConnection = 'mongodb://localhost:27017/chestnut';
+const server = initChestnut({ port: PORT, models: models, dbConnection: dbConnection });
