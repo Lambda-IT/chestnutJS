614bb66a16f52e5fe3171760c3ac38efc673dda6
SusanneBoghossian <susanne.boghossian@lambda-it.ch>
2017-11-17 16:05:21 +0100

    ng-material, new layout, table sort

--- chestnut/client/src/app/models/components/model-view/model-view.component.ts
+++ chestnut/client/src/app/models/components/model-view/model-view.component.ts
@@ -1,19 +1,47 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {
+    Component,
+    OnInit,
+    Input,
+    Output,
+    EventEmitter,
+    OnDestroy,
+    OnChanges,
+    ViewChild,
+    AfterViewInit,
+} from '@angular/core';
 import { ModelDescription, PropertyDescription } from '../../../../../../common/metadata';
+import { MatTableDataSource, MatSort } from '@angular/material';
+
 @Component({
     selector: 'model-view',
     templateUrl: 'model-view.component.html',
     styleUrls: ['model-view.scss'],
 })
-export class ModelViewComponent implements OnInit {
+export class ModelViewComponent implements OnInit, OnDestroy, OnChanges, AfterViewInit {
     @Input() modelView: ModelDescription;
     @Input() modelData: any;
     @Output() setColumnVisibleClicked = new EventEmitter();
+
+    propColumns: string[];
+    tableData;
+    @ViewChild(MatSort) sort: MatSort;
 
     setColumnVisible(propertyName: string) {
         this.setColumnVisibleClicked.emit(propertyName);
     }
 
     constructor() {}
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        this.tableData = new MatTableDataSource(this.modelData);
+        this.propColumns = this.modelView.properties.filter(p => !p.hidden).map(p => p.name);
+    }
+
+    ngAfterViewInit() {
+        this.tableData.sort = this.sort;
+    }
+    ngOnDestroy(): void {}
+
+    ngOnChanges() {
+        this.propColumns = this.modelView.properties.filter(p => !p.hidden).map(p => p.name);
+    }
 }
