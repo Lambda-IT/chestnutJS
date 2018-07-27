0b09d7380736f477c8c62985851db2334c22ce61
Roger Rudin <roger.rudin@lambda-it.ch>
2018-07-27 15:48:39 +0200

    tslint, code formating rules and vscode settings added, code formatted

--- chestnut/client/src/app/app.module.ts
+++ chestnut/client/src/app/app.module.ts
@@ -2,23 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import {
-  MatButtonModule, MatToolbarModule, MatIconModule, MatMenuModule, MatListModule,
-  MatSidenavModule, MatSelectModule, MatTableModule, MatSortModule, MatInputModule,
-  MatCheckboxModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatListModule,
+    MatSidenavModule,
+    MatSelectModule,
+    MatTableModule,
+    MatSortModule,
+    MatInputModule,
+    MatCheckboxModule,
 } from '@angular/material';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule, NoopAnimationsModule,
-    MatButtonModule, MatToolbarModule, MatIconModule, MatMenuModule, MatListModule,
-    MatSidenavModule, MatSelectModule, MatTableModule, MatSortModule, MatInputModule,
-    MatCheckboxModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        NoopAnimationsModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatMenuModule,
+        MatListModule,
+        MatSidenavModule,
+        MatSelectModule,
+        MatTableModule,
+        MatSortModule,
+        MatInputModule,
+        MatCheckboxModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
