4b413d935dc8637b0103f5bfba19418835fd3102
Roger Rudin <roger.rudin@lambda-it.ch>
2018-07-27 15:38:36 +0200

    angular material added

--- chestnut/client/src/app/app.module.ts
+++ chestnut/client/src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatButtonModule, MatToolbarModule, MatIconModule, MatMenuModule, MatListModule,
+  MatSidenavModule, MatSelectModule, MatTableModule, MatSortModule, MatInputModule,
+  MatCheckboxModule,
+} from '@angular/material';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +13,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, NoopAnimationsModule,
+    MatButtonModule, MatToolbarModule, MatIconModule, MatMenuModule, MatListModule,
+    MatSidenavModule, MatSelectModule, MatTableModule, MatSortModule, MatInputModule,
+    MatCheckboxModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
