768022a6f8089bd509021a2cbd9a5658402c0331
Roger Rudin <roger.rudin@lambda-it.ch>
2018-07-31 16:59:06 +0200

    unused code removed

--- chestnut/client/src/app/static/static.routing.ts
+++ chestnut/client/src/app/static/static.routing.ts
@@ -2,11 +2,6 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 
 export const staticRoutes: Routes = [
-    // {
-    //     path: '',
-    //     redirectTo: '/about',
-    //     pathMatch: 'full',
-    // },
     {
         path: 'about',
         component: AboutComponent,
