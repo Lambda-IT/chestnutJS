a489a455386af5c88c46959a7eaba416394244a2
Edi Spring <edi-tech@lambda-it.ch>
2018-10-15 15:50:32 +0200

    fixed postinstall

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+export { initChestnut, Chestnut } from './server/src';
+export * from './common/metadata';
+export { Response, Request } from './server/src/middleware';
+export { editor, hidden, excludeFromModel } from './server/src/decorators';
