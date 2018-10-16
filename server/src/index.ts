b3d15be6945150cb34d34dd7dd362eace5572c9f
Edi Spring <edi-tech@lambda-it.ch>
2018-10-16 16:06:45 +0200

    comment useNewUrlParser

--- server/src/index.ts
+++ server/src/index.ts
@@ -92,7 +92,7 @@ export async function initChestnut(
         {
             useMongoClient: true,
             /* other options */
-            useNewUrlParser: true,
+            // useNewUrlParser: true,
         },
         options.modelPrefix
     );
