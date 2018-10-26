0483041825efdb207724858602726698187ab5bb
Roger Rudin <roger.rudin@lambda-it.ch>
2018-10-26 17:53:21 +0200

    title on tinymce styled

--- client/src/app/shared/formly/tinymce.component.ts
+++ client/src/app/shared/formly/tinymce.component.ts
@@ -3,8 +3,10 @@ import { FieldType } from '@ngx-formly/core';
 
 @Component({
     selector: 'app-formly-tinymce',
+    styleUrls: ['./tinymce.component.scss'],
     template: `
-        <editor [inline]="true" [init]="{plugins: 'link'}" [formControl]="formControl" [formlyAttributes]="field"></editor>
+            <mat-label class="label">{{field.key}}</mat-label>
+            <editor [inline]="true" [init]="{plugins: 'link'}" [formControl]="formControl"></editor>
     `,
 })
 export class FormlyTinyMCEComponent extends FieldType {}
