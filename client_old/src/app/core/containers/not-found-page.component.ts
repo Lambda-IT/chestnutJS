47fb88b933a4c2ba2e4aa1d6892236caca4b3e7c
danielmargreth <daniel.margreth@lambda-it.ch>
2019-05-27 10:56:54 +0200

    removed old client project

--- client_old/src/app/core/containers/not-found-page.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'not-found-page',
-    template: `<h2>This page does not exist.</h2>`,
-})
-export class NotFoundPageComponent {
-    constructor() {}
-}
--- client_old/src/app/shared/model-view-data.ts
+++
@@ -1,6 +0,0 @@
-import { ModelDescription } from '../../../../common/metadata';
-
-export interface ModelViewData {
-    modelView: ModelDescription;
-    modelData: [{}];
-}
