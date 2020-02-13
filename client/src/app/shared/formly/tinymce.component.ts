e7277cb158c1a4d9fe551a0fbf5c439a4587e73a
Philipp Schaerer <philipp.schaerer@lambda-it.ch>
2020-02-13 16:20:07 +0100

    Fixed tinymce reference

--- client/src/app/shared/formly/tinymce.component.ts
+++ client/src/app/shared/formly/tinymce.component.ts
@@ -6,7 +6,11 @@ import { FieldType } from '@ngx-formly/core';
     styleUrls: ['./tinymce.component.scss'],
     template: `
         <mat-label class="label">{{ field.key }}</mat-label>
-        <editor [inline]="true" [init]="{ plugins: 'link' }" [formControl]="formControl"></editor>
+        <editor
+            [inline]="false"
+            [init]="{ base_url: 'assets/tinymce', plugins: 'link', height: 400, max_height: 700 }"
+            [formControl]="formControl"
+        ></editor>
     `,
 })
 export class FormlyTinyMCEComponent extends FieldType {}
