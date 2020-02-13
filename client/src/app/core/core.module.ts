951e173ef2a293a4314b0c789dfd3d7ad6fd5dee
Philipp Schaerer <philipp.schaerer@lambda-it.ch>
2020-02-13 16:21:44 +0100

    Added layout container and link to home

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
 import { SharedModule } from '@shared/shared.module';
-import { LoginDialogComponent } from './login-dialog/login-dialog.component';
 import { FormsModule } from '@angular/forms';
+
+import { HeaderComponent } from './header/header.component';
+import { LoginDialogComponent } from './login-dialog/login-dialog.component';
+import { LayoutContainer } from './layout-container/layout-container';
 
 @NgModule({
     imports: [CommonModule, SharedModule, FormsModule],
-    declarations: [HeaderComponent, LoginDialogComponent],
-    exports: [HeaderComponent, LoginDialogComponent],
+    declarations: [HeaderComponent, LayoutContainer, LoginDialogComponent],
+    exports: [HeaderComponent, LayoutContainer, LoginDialogComponent],
 })
 export class CoreModule {}
---
+++ client/src/app/core/layout-container/layout-container.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-layout-container',
+    styleUrls: ['./layout-container.scss'],
+    template: '<ng-content></ng-content>',
+})
+export class LayoutContainer {}
